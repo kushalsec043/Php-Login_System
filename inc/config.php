4b76f83a22e35ab99644571e5b9cb06a74df510f
Your Name <kushalsec043@gmail.com>
2020-06-15 15:44:49 -0500

    Editted and added new files

---
+++ inc/config.php
@@ -0,0 +1,9 @@
+
+<?php
+
+//If there is no definde config 
+ if(!defined('__CONFIG__'))
+ {
+ 	exit('You donot have a config file');
+ }
+ ?>
--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php echo "WELCOME TO LOGIN PAGE DJJJJJJJJJ!!!!!" ; ?>
+
+
+<?php
+define('__CONFIG__', true);
+ require_once "inc/config.php" ;
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -16,37 +22,23 @@
   <body>
 
   	<div class="uk-section uk-container">
-  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-			<form class="uk-form-stacked js-login">
 
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Email</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
-			        </div>
-			    </div>
 
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Password</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
-			        </div>
-			    </div>
+			<?php
+         echo "helloooooooo Today is : ";
+         echo date("Y m d");
+       ?>
 
-			    <div class="uk-margin">
-			        <button class="uk-button uk-button-default" type="submit">Login</button>
-			    </div>
+       <p>
+       <a href = "/php-login/login.php">Login</a>
+       <a href = "/php-login/register.php">Register</a>
+       </p>
 
-			</form>
-  		</div>
+
   	</div>
 
-    <!-- jQuery is required -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-    <!-- UIkit JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js"></script>
+<? php require_once "inc/footer.php">
+    
 
   </body>
 </html>
